e6725da75a80d6e7b3bc6cf03da65ab5b7a59365
Bakiyama <htt.ikumi@gmail.com>
2021-02-03 15:50:12 +0900

    ルーティング・コントローラーを実装

--- app/controllers/purposes_controller.rb
+++ app/controllers/purposes_controller.rb
@@ -1,4 +1,10 @@
 class PurposesController < ApplicationController
   def index
   end
+
+  def new
+  end
+
+  def create
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get 'purposes/index'
   root to: "purposes#index"
+  resources :purposes, only: [:index, :new, :create]
 end
