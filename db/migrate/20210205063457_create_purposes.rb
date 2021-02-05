809ce772ad242375bf5960d9d6d77c481df86ea4
Bakiyama <htt.ikumi@gmail.com>
2021-02-05 18:35:49 +0900

    マイグレーションを記述

--- db/migrate/20210205063457_create_purposes.rb
+++ db/migrate/20210205063457_create_purposes.rb
@@ -1,7 +1,9 @@
 class CreatePurposes < ActiveRecord::Migration[6.0]
   def change
     create_table :purposes do |t|
-
+      t.string :name,   null: false
+      t.integer :unit,    null: false
+      t.references :user,  foreign_key: true
       t.timestamps
     end
   end
