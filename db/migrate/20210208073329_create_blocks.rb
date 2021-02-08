bc71da81f148ff9da81395086e036590a5709a9d
Bakiyama <htt.ikumi@gmail.com>
2021-02-08 16:39:36 +0900

    blockテーブルのマイグレーションを追加

--- db/migrate/20210208073329_create_blocks.rb
+++ db/migrate/20210208073329_create_blocks.rb
@@ -1,7 +1,9 @@
 class CreateBlocks < ActiveRecord::Migration[6.0]
   def change
     create_table :blocks do |t|
-
+      t.integer :value,   null: false
+      t.references :user,  foreign_key: true
+      t.references :purpose,  foreign_key: true
       t.timestamps
     end
   end
