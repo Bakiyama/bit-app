0ad0f2786f37a0db4b055e7eea6eccac4f3c2625
Bakiyama <htt.ikumi@gmail.com>
2021-02-12 19:10:58 +0900

    アクティブハッシュのvalueモデルを作成

---
+++ app/models/value.rb
@@ -0,0 +1,14 @@
+class Value < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '---' }, 
+    { id: 2, name: 'いつもと同じ' }, 
+    { id: 3, name: 'いつもの2倍できた' },
+    { id: 4, name: 'いつもの2.5倍できた' },
+    { id: 5, name: 'いつもの3倍できた' }, 
+    { id: 6, name: 'いつもの3.5倍できた' }, 
+    { id: 7, name: 'いつもの4倍できた' }
+    { id: 8, name: 'いつもの4.5倍できた' }
+    { id: 9, name: 'いつもの5倍できた' }
+  ]
+
+end
