b43d0760f8a4b52749d7052dd16cdc39fb8d295e
Bakiyama <htt.ikumi@gmail.com>
2021-02-18 12:26:56 +0900

    ルーティングを削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'purposes/index'
   root to: "purposes#index"
   resources :purposes, only: [:index, :new, :create, :show] do
     resources :blocks, only: [:create]
